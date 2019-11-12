b0da7dfa41e55cd8843baebe5b76f78d8b3fd9c8
Alexander Lau <alexanderlau25@hotmail.com>
2019-11-12 23:16:16 +1100

    [bunnings.spec.js] preliminary varible setups, first test

--- src/tests/bunnings.spec.js
+++ src/tests/bunnings.spec.js
@@ -1,18 +1,54 @@
-const ProductsPage = require("./node_modules");
+const webdriver = require("selenium-webdriver");
+
+const ProductsPage = require("@/product_details");
+const ProductDetailsPage = require("@/product_details");
+const WishlistPage = require("@/wishlist");
+
+const driver = new webdriver.Builder()
+    .forBrowser("chrome")
+    .setLoggingPrefs(pref)
+    .build();
+
+
 
 describe(
     "bunnings test",
     function ()
     {
+        let 
+            products_page,
+            product_details_page,
+            wishlist_page;
+            
+
+
         beforeAll(
             async function ()
             {
-                
+                products_page = new ProductsPage({
+                    driver
+                });
+                product_details_page = new ProductDetailsPage({
+                    driver
+                });
+                wishlist_page = new WishlistPage({
+                    driver
+                });
             }
         );
 
         describe(
-            ""
+            "ProductsPage",
+            function ()
+            {
+                it(
+                    "can load page",
+                    async function ()
+                    {
+                        await products_page.loadPage();
+                    }
+                );
+            }
         );
     }
 );
