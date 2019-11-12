8b7f63313a49e68d4821f69c0614feed8ff9b42f
Alexander Lau <alexanderlau25@hotmail.com>
2019-11-13 08:17:54 +1100

    [wishlist.js] file now functional

--- src/pages/wishlist.js
+++ src/pages/wishlist.js
@@ -1,21 +1,29 @@
-class WishlistPage
+// webdriver imported by BasePage
+const BasePage = require("./base_page");
+
+class WishlistPage extends BasePage
 {
-    constructor (
+    constructor ({
         driver,
         target_url = ""
-    )
+    })
     {
-        this.driver = driver;
-        this.target_url = target_url;
+        super({
+            driver,
+            target_url
+        });
     }
 
 
 
-    async getProductsLength ()
+    async getProductsCount ()
     {
-        let element = this.webdriver.By.css("strong[data-role=wish-list-item-count]");
-        let text = await this.driver.findElement(element).getText();
-        let count = text.match(/\d+/);
+        let selector = {
+            css: "strong[data-role=wish-list-item-count]"
+        };
+        let element = await this.driver.findElement(selector);
+        let text = await element.getText();
+        let count = text.match(/\d+/)[0];
         return count;
     }
 }
